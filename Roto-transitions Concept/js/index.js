1c8fb12fddbbdb49655410fe66f8b38ddce02444
AbdellatifAhammad <abdellatif.ahammad@gmail.com>
2023-09-25 18:21:33 +0100

    chore: setup the basic animation of the clipped images

--- Roto-transitions Concept/js/index.js
+++ Roto-transitions Concept/js/index.js
@@ -6,3 +6,22 @@
  * Description:
 */
 
+var currentImage = 1;
+var totalImages = 2;
+
+
+
+function switch_images() {
+    for (let index = 1; index < 4; index++) {
+        var nextImage  = currentImage+1;
+        if (currentImage==totalImages){
+            nextImage = 1;
+        }
+        var slice = document.getElementById(`slice-${index}`);
+        var img1 = document.getElementById(`s${index}-img${currentImage}`);
+        var img2 = document.getElementById(`s${index}-img${nextImage}`);
+        slice.style.animationPlayState = 'running';
+        img1.classList.add('disappear-out');
+        img2.classList.add('zoomed-in');
+    }
+}
