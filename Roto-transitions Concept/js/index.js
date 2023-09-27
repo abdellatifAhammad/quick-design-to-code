ddbfe859965e58e281a951f6269be439fad4dc3a
AbdellatifAhammad <abdellatif.ahammad@gmail.com>
2023-09-27 16:40:27 +0100

    chore: complete the text animation part, add some reponsivity to the design

--- Roto-transitions Concept/js/index.js
+++ Roto-transitions Concept/js/index.js
@@ -2,18 +2,18 @@
  * Project Name: Roto-transitions Concept
  * Version: 1.0 
  * Author: Abdellatif Ahammad 
- * Author URI: 
- * Description:
+ * Author Github: https://github.com/abdellatifAhammad
+ * Description: Roto-transitions Concept
 */
 
+const SLICES_NUMBER = 3;
+const TOTAL_IMAGES = 5;
 var currentImage = 1;
-var totalImages = 4;
 var btnClicks = 0;
 
-
 function clear_previous() {
-    for (let i = 1; i < totalImages; i++) {
-        for (let j = 1; j < totalImages + 1; j++) {
+    for (let i = 1; i < SLICES_NUMBER + 1; i++) {
+        for (let j = 1; j < TOTAL_IMAGES + 1; j++) {
             var image = document.getElementById(`s${i}-img${j}`);
             image.classList.remove('zoomed-in', 'disappear-out');
         }
@@ -24,10 +24,12 @@ function clear_previous() {
 function switch_images() {
     btn = document.getElementsByClassName("switch-btn")[0];
     btn.classList.add("switch-btn-circle");
+
+    // sliced pictures animation
     setTimeout(() => {
-        if (currentImage < totalImages) {
-            if (currentImage == totalImages - 1) {
-                for (let index = 1; index < totalImages; index++) {
+        if (currentImage < TOTAL_IMAGES) {
+            if (currentImage == TOTAL_IMAGES - 1) {
+                for (let index = 1; index < SLICES_NUMBER + 1; index++) {
                     var prev_image = document.getElementById(`s${index}-img${currentImage}`);
                     var next_image = document.getElementById(`s${index}-img${currentImage + 1}`);
                     prev_image.classList.remove('disappear-out');
@@ -40,7 +42,7 @@ function switch_images() {
                     currentImage = 1;
                 }, 1300);
             } else {
-                for (let index = 1; index < totalImages; index++) {
+                for (let index = 1; index < SLICES_NUMBER + 1; index++) {
                     var prev_image = document.getElementById(`s${index}-img${currentImage}`);
                     var next_image = document.getElementById(`s${index}-img${currentImage + 1}`);
                     prev_image.classList.remove('disappear-out');
@@ -54,25 +56,50 @@ function switch_images() {
         btn.classList.remove("switch-btn-circle");
     }, 500);
 
+
+    // text animation
     setTimeout(() => {
         let first_spans = document.querySelectorAll("div.text-spin-animation-wrapper > span");
         let last_spans = document.querySelectorAll("div.text-cube-spin-animation-wrapper > span");
-        if (btnClicks < totalImages - 2) {
-            first_spans.forEach((span, index) => {
-                span.style.transform = `translateY(-${(1.75 * (btnClicks + 1))}em)`;
-            });
-            last_spans.forEach((span, index) => {
-                span.style.transform = `translateY(${(1.5 * (btnClicks + 1))}em)`;
-            });
-            btnClicks++;
+        let window_width = window.innerWidth;
+        if (window_width > 768) {
+            if (btnClicks < TOTAL_IMAGES - 2) {
+                first_spans.forEach((span, index) => {
+                    span.style.transform = `translateY(-${(1.75 * (btnClicks + 1))}em)`;
+                });
+                last_spans.forEach((span, index) => {
+                    span.style.transform = `translateY(${(1.5 * (btnClicks + 1))}em)`;
+                });
+                btnClicks++;
+            } else {
+                btnClicks = 0;
+                first_spans.forEach((span, index) => {
+                    span.style.transform = `translateY(-${(1.75 * (btnClicks))}em)`;
+                });
+                last_spans.forEach((span, index) => {
+                    span.style.transform = `translateY(${(1.5 * (btnClicks))}em)`;
+                });
+            }
         } else {
-            btnClicks = 0;
-            first_spans.forEach((span, index) => {
-                span.style.transform = `translateY(-${(1.75 * (btnClicks))}em)`;
-            });
-            last_spans.forEach((span, index) => {
-                span.style.transform = `translateY(${(1.5 * (btnClicks))}em)`;
-            });
+            if (btnClicks < TOTAL_IMAGES - 2) {
+                first_spans.forEach((span, index) => {
+                    span.style.transform = `translateY(-${(1.5 * (btnClicks + 1))}em)`;
+                });
+                last_spans.forEach((span, index) => {
+                    span.style.transform = `translateY(${(1.5 * (btnClicks + 1))}em)`;
+                });
+                btnClicks++;
+            } else {
+                btnClicks = 0;
+                first_spans.forEach((span, index) => {
+                    span.style.transform = `translateY(-${(1.5 * (btnClicks))}em)`;
+                });
+                last_spans.forEach((span, index) => {
+                    span.style.transform = `translateY(${(1.5 * (btnClicks))}em)`;
+                });
+            }
         }
+        btn.style.rotate = `${360 * (btnClicks + 1)}deg`;
     }, 500);
+
 }
